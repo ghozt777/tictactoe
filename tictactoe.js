ca0373dea7392355ba2cc42561e6ebbd9059de91
ghozt777 <ghozt777.code@gmail.com>
2021-07-10 13:52:06 +0530

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,67 +1,65 @@
-var readlineSync = require('readline-sync')
-var c1 = [" ","|"," ","|"," "]
-var c2 = ["-","+","-","+","-"]
-var c3 = [" ","|"," ","|"," "]
-var c4 = ["-","+","-","+","-"]
-var c5 = [" ","|"," ","|"," "]
-var userPos = []
-var cpuPos = []
-var userCount = 0
-var cpuCount =0
-const print = console.log
-const chalk = require('chalk')
+var readlineSync = require('readline-sync');
+var c1 = [" ","|"," ","|"," "];
+var c2 = ["-","+","-","+","-"];
+var c3 = [" ","|"," ","|"," "];
+var c4 = ["-","+","-","+","-"];
+var c5 = [" ","|"," ","|"," "];
+var userPos = [];
+var cpuPos = [];
+var userCount = 0;
+var cpuCount =0;
 function printGrid()
 {
-    print(chalk.yellow(...c1))
-    print(chalk.red(...c2))
-    print(chalk.red(...c3))
-    print(chalk.red(...c4))
-    print(chalk.yellow(...c5))
+console.log(...c1);
+console.log(...c2);
+console.log(...c3);
+console.log(...c4);
+console.log(...c5);
 }
 function takeUserInput()
 {
     if((9-userCount+cpuCount)>0)
     {
 
-        var temp = readlineSync.question("enter the position(1-9): ")
-        var condition = checkForIllegalMoves("user",temp)
+        var temp = readlineSync.question("enter the position(1-9): ");
+        var condition = checkForIllegalMoves("user",temp);
         while(condition==1)
         {
-            print("Illegal Move Please Re-enter: ")
-            var temp = readlineSync.question("enter the position(1-9): ")
-            var condition = checkForIllegalMoves("user",temp)
+            console.log("Illegal Move Please Re-enter: ");
+            var temp = readlineSync.question("enter the position(1-9): ");
+            var condition = checkForIllegalMoves("user",temp);
         }
-        userPos[temp-1]=true
-        updateBoard("user",temp)
-        printGrid()
-        userCount++
+        userPos[temp-1]=true;
+        updateBoard("user",temp);
+        printGrid();
+        userCount++;
 
     }
     else
     {
-        print(" err no moves left")
+        console.log(" err no moves left");
     }
 }
 function takeCpuInput()
 {
   if(9 -(userCount+cpuCount) > 0)
   {
-    var temp = Math.floor((Math.random() * 9) + 1)
-    var condition = checkForIllegalMoves("cpu",temp)
+    var temp = Math.floor((Math.random() * 9) + 1);
+    var condition = checkForIllegalMoves("cpu",temp);
     while(condition==1)
     {
-        var temp = Math.floor((Math.random() * 9) + 1)
-        var condition = checkForIllegalMoves("cpu",temp)  
+        var temp = Math.floor((Math.random() * 9) + 1);
+        var condition = checkForIllegalMoves("cpu",temp);  
     }
-    print("CPUs Move ---> Position: "+ (temp))
-    cpuPos[temp-1]=true
-    cpuCount++
-    updateBoard("cpu",temp)
-    printGrid()
+    console.log("CPUs Move ---> Position: "+ (temp));
+    cpuPos[temp-1]=true;
+    cpuCount++;
+    updateBoard("cpu",temp);
+    printGrid();
   }
   else
   {
-    print(" err no move left")
+    console.log(" err no move left");
   }
 }
 
@@ -69,11 +67,19 @@ function updateBoard(player,pos)
 {
   if(player === chalk.green("user"))
   {
+<<<<<<< HEAD
     let symbol = ("O");
   }
   if(player === "cpu")
   {
     let symbol = "X";
+=======
+    symbol = "O";
+  }
+  if(player === "cpu")
+  {
+    symbol = "X";
+>>>>>>> parent of 3c4e61c (Update tictactoe.js)
   }
   if(pos == 1){c1[0]=symbol;}
   if(pos == 2){c1[2]=symbol;}
@@ -93,10 +99,10 @@ function checkForIllegalMoves(player,val)
         {
             if(cpuPos[val-1]==true||userPos[val-1]==true)
             {
-                return 1
+                return 1;
             }
         }
-        return 0
+        return 0;
     }
     if(player === "cpu")
     {
@@ -104,48 +110,48 @@ function checkForIllegalMoves(player,val)
         {
             if(userPos[val-1]==true||cpuPos[val-1]==true)
             {
-                return 1
+                return 1;
             }
         }
-        return 0
+        return 0;
     }
 }
 function checkCondition()
 {
-    var userMessage = "congrats u won !"
-    var cpuMessage = "opps looks like u just lost ! well better luck next time ...."
+    var userMessage = "congrats u won !";
+    var cpuMessage = "opps looks like u just lost ! well better luck next time ....";
 
     // USER WINNING CONDITIONS
 
-    if(userPos[0]==true&&userPos[1]==true&&userPos[2]==true){print(userMessage); return 1}  // row ;
-    if(userPos[3]==true&&userPos[4]==true&&userPos[5]==true){print(userMessage); return 1}  // row 2 
-    if(userPos[6]==true&&userPos[7]==true&&userPos[8]==true){print(userMessage); return 1}  // row 3
-    if(userPos[0]==true&&userPos[3]==true&&userPos[6]==true){print(userMessage); return 1}  // column 1
-    if(userPos[1]==true&&userPos[4]==true&&userPos[7]==true){print(userMessage); return 1}  // column 2 
-    if(userPos[2]==true&&userPos[5]==true&&userPos[8]==true){print(userMessage); return 1}  // column 3
-    if(userPos[0]==true&&userPos[4]==true&&userPos[8]==true){print(userMessage); return 1}  // diagonal 1
-    if(userPos[2]==true&&userPos[4]==true&&userPos[6]==true){print(userMessage); return 1}  // diagonal 2
+    if(userPos[0]==true&&userPos[1]==true&&userPos[2]==true){console.log(userMessage); return 1;}  // row 1
+    if(userPos[3]==true&&userPos[4]==true&&userPos[5]==true){console.log(userMessage); return 1;}  // row 2 
+    if(userPos[6]==true&&userPos[7]==true&&userPos[8]==true){console.log(userMessage); return 1;}  // row 3
+    if(userPos[0]==true&&userPos[3]==true&&userPos[6]==true){console.log(userMessage); return 1;}  // column 1
+    if(userPos[1]==true&&userPos[4]==true&&userPos[7]==true){console.log(userMessage); return 1;}  // column 2 
+    if(userPos[2]==true&&userPos[5]==true&&userPos[8]==true){console.log(userMessage); return 1;}  // column 3
+    if(userPos[0]==true&&userPos[4]==true&&userPos[8]==true){console.log(userMessage); return 1;}  // diagonal 1
+    if(userPos[2]==true&&userPos[4]==true&&userPos[6]==true){console.log(userMessage); return 1;}  // diagonal 2
 
     // CPU WINNING CONDITIONS
 
-    if(cpuPos[0]==true&&cpuPos[1]==true&&cpuPos[2]==true){print(cpuMessage); return 1}  // row 1
-    if(cpuPos[3]==true&&cpuPos[4]==true&&cpuPos[5]==true){print(cpuMessage); return 1}  // row 2 
-    if(cpuPos[6]==true&&cpuPos[7]==true&&cpuPos[8]==true){print(cpuMessage); return 1}  // row 3
-    if(cpuPos[0]==true&&cpuPos[3]==true&&cpuPos[6]==true){print(cpuMessage); return 1}  // column 1
-    if(cpuPos[1]==true&&cpuPos[4]==true&&cpuPos[7]==true){print(cpuMessage); return 1}  // column 2 
-    if(cpuPos[2]==true&&cpuPos[5]==true&&cpuPos[8]==true){print(cpuMessage); return 1}  // column 3
-    if(cpuPos[0]==true&&cpuPos[4]==true&&cpuPos[8]==true){print(cpuMessage); return 1}  // diagonal 1
-    if(cpuPos[2]==true&&cpuPos[4]==true&&cpuPos[6]==true){print(cpuMessage); return 1}  // diagonal 2
+    if(cpuPos[0]==true&&cpuPos[1]==true&&cpuPos[2]==true){console.log(cpuMessage); return 1;}  // row 1
+    if(cpuPos[3]==true&&cpuPos[4]==true&&cpuPos[5]==true){console.log(cpuMessage); return 1;}  // row 2 
+    if(cpuPos[6]==true&&cpuPos[7]==true&&cpuPos[8]==true){console.log(cpuMessage); return 1;}  // row 3
+    if(cpuPos[0]==true&&cpuPos[3]==true&&cpuPos[6]==true){console.log(cpuMessage); return 1;}  // column 1
+    if(cpuPos[1]==true&&cpuPos[4]==true&&cpuPos[7]==true){console.log(cpuMessage); return 1;}  // column 2 
+    if(cpuPos[2]==true&&cpuPos[5]==true&&cpuPos[8]==true){console.log(cpuMessage); return 1;}  // column 3
+    if(cpuPos[0]==true&&cpuPos[4]==true&&cpuPos[8]==true){console.log(cpuMessage); return 1;}  // diagonal 1
+    if(cpuPos[2]==true&&cpuPos[4]==true&&cpuPos[6]==true){console.log(cpuMessage); return 1;}  // diagonal 2
 }
-var gameCondition = checkCondition()
+var gameCondition = checkCondition();
 while(gameCondition!=1)
 {
     if(userCount+cpuCount==9)
     {
-        print("Its a draw")
-        break
+        console.log("Its a draw");
+        break;
     }
-    takeUserInput()
-    takeCpuInput()
-    var gameCondition = checkCondition()
+    takeUserInput();
+    takeCpuInput();
+    var gameCondition = checkCondition();
 }
